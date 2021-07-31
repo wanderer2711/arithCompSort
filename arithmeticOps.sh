77f7b8efe502cfb2f569717e8606f63c8cbf7922
suhridh <suhridhsundaram@gmail.com>
2021-07-31 21:49:07 +0530

    added dict

--- arithmeticOps.sh
+++ arithmeticOps.sh
@@ -6,3 +6,8 @@ sum=$(( $num1+$num2*$num3 ))
 ops2=$(( $num1*$num2+$num3 ))
 ops3=$(( $num3+$num1/$num2 ))
 ops4=$(( $num1%$num2+$num3 ))
+declare -A computation
+computation[add]=$sum
+computation[mul]=$ops2
+computation[div]=$ops3
+computation[mod]=$ops4
